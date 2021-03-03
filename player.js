31afcfdac3762f36efc6f80900a28c215492b3e7
slavko <slavkoj6@gmail.com>
2021-03-03 17:35:19 +0100

    player v1, (playing songs, next and prev)

---
+++ player.js
@@ -0,0 +1,423 @@
+const albumData = {
+  id: 915782,
+  title: "Queen II (Deluxe Edition 2011 Remaster)",
+  upc: "602527642499",
+  link: "http://www.deezer.com/album/915782",
+  share:
+    "https://www.deezer.com/album/915782?utm_source=deezer&utm_content=album-915782&utm_term=0_1614763397&utm_medium=web",
+  cover: "http://api.deezer.com/album/915782/image",
+  cover_small: "http://e-cdn-images.deezer.com/images/cover/2b8899e10a2c8e7021b907697b2e5010/56x56-000000-80-0-0.jpg",
+  cover_medium:
+    "http://e-cdn-images.deezer.com/images/cover/2b8899e10a2c8e7021b907697b2e5010/250x250-000000-80-0-0.jpg",
+  cover_big: "http://e-cdn-images.deezer.com/images/cover/2b8899e10a2c8e7021b907697b2e5010/500x500-000000-80-0-0.jpg",
+  cover_xl: "http://e-cdn-images.deezer.com/images/cover/2b8899e10a2c8e7021b907697b2e5010/1000x1000-000000-80-0-0.jpg",
+  md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+  genre_id: 152,
+  genres: { data: [{ id: 152, name: "Rock", picture: "http://api.deezer.com/genre/152/image", type: "genre" }] },
+  label: "EMI",
+  nb_tracks: 16,
+  duration: 3579,
+  fans: 24256,
+  rating: 0,
+  release_date: "2011-03-14",
+  record_type: "album",
+  available: true,
+  tracklist: "http://api.deezer.com/album/915782/tracks",
+  explicit_lyrics: false,
+  explicit_content_lyrics: 0,
+  explicit_content_cover: 0,
+  contributors: [
+    {
+      id: 412,
+      name: "Queen",
+      link: "http://www.deezer.com/artist/412",
+      share:
+        "https://www.deezer.com/artist/412?utm_source=deezer&utm_content=artist-412&utm_term=0_1614763397&utm_medium=web",
+      picture: "http://api.deezer.com/artist/412/image",
+      picture_small:
+        "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/56x56-000000-80-0-0.jpg",
+      picture_medium:
+        "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/250x250-000000-80-0-0.jpg",
+      picture_big:
+        "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/500x500-000000-80-0-0.jpg",
+      picture_xl:
+        "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/1000x1000-000000-80-0-0.jpg",
+      radio: true,
+      tracklist: "http://api.deezer.com/artist/412/top?limit=50",
+      type: "artist",
+      role: "Main",
+    },
+  ],
+  artist: {
+    id: 412,
+    name: "Queen",
+    picture: "http://api.deezer.com/artist/412/image",
+    picture_small:
+      "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/56x56-000000-80-0-0.jpg",
+    picture_medium:
+      "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/250x250-000000-80-0-0.jpg",
+    picture_big:
+      "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/500x500-000000-80-0-0.jpg",
+    picture_xl:
+      "http://e-cdn-images.deezer.com/images/artist/0b17b99897d17ceb7027ed57cdbb7044/1000x1000-000000-80-0-0.jpg",
+    tracklist: "http://api.deezer.com/artist/412/top?limit=50",
+    type: "artist",
+  },
+  type: "album",
+  tracks: {
+    data: [
+      {
+        id: 9996961,
+        readable: true,
+        title: "Procession",
+        title_short: "Procession",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996961",
+        duration: 73,
+        rank: 208532,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-3.deezer.com/stream/c-3b11dbb6616dce3c9879804ed1171e51-18.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996962,
+        readable: true,
+        title: "Father To Son",
+        title_short: "Father To Son",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996962",
+        duration: 373,
+        rank: 264748,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-9.deezer.com/stream/c-90e13631740c58dc8694adeb514e7b84-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996963,
+        readable: true,
+        title: "White Queen (As It Began)",
+        title_short: "White Queen (As It Began)",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996963",
+        duration: 273,
+        rank: 211839,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-0.deezer.com/stream/c-02cdc15a97b4a8f89fd778e5ea523200-17.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996964,
+        readable: true,
+        title: "Some Day One Day",
+        title_short: "Some Day One Day",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996964",
+        duration: 262,
+        rank: 233265,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-9.deezer.com/stream/c-9cdc4620d8d64a661e1d61faad38d7ee-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996965,
+        readable: true,
+        title: "The Loser In The End",
+        title_short: "The Loser In The End",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996965",
+        duration: 241,
+        rank: 224473,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-6.deezer.com/stream/c-67ceb09c5f6e8b4e9db5eb5f06954457-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996966,
+        readable: true,
+        title: "Ogre Battle",
+        title_short: "Ogre Battle",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996966",
+        duration: 248,
+        rank: 220073,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-c.deezer.com/stream/c-c29a5c8829785e5430e6164387dc80c6-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996967,
+        readable: true,
+        title: "The Fairy Feller's Master-Stroke",
+        title_short: "The Fairy Feller's Master-Stroke",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996967",
+        duration: 160,
+        rank: 234770,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-b.deezer.com/stream/c-bb47a28f435238eb63fe61e56aaa8701-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996968,
+        readable: true,
+        title: "Nevermore",
+        title_short: "Nevermore",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996968",
+        duration: 78,
+        rank: 244114,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-3.deezer.com/stream/c-3815bb2553375ef180b58c4935446854-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996969,
+        readable: true,
+        title: "The March Of The Black Queen",
+        title_short: "The March Of The Black Queen",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996969",
+        duration: 392,
+        rank: 253610,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-b.deezer.com/stream/c-b4595c4705b6b062a1422549fc5e8196-17.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996970,
+        readable: true,
+        title: "Funny How Love Is",
+        title_short: "Funny How Love Is",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996970",
+        duration: 169,
+        rank: 189332,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-7.deezer.com/stream/c-7347e6b9e5fba4dff7f00cf9015c0029-18.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996971,
+        readable: true,
+        title: "Seven Seas Of Rhye",
+        title_short: "Seven Seas Of Rhye",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996971",
+        duration: 169,
+        rank: 259833,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-e.deezer.com/stream/c-e23edd3dd97f6860d0073b6024cef6f8-16.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996972,
+        readable: true,
+        title: "See What A Fool I've Been",
+        title_short: "See What A Fool I've Been",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996972",
+        duration: 262,
+        rank: 187509,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-b.deezer.com/stream/c-b32a98c50cbd1345933b2fcd196dfa0c-17.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996973,
+        readable: true,
+        title: "White Queen (As It Began)",
+        title_short: "White Queen (As It Began)",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996973",
+        duration: 332,
+        rank: 214253,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-e.deezer.com/stream/c-e2417c3daf3f6a721fb63079f4c2676b-17.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996974,
+        readable: true,
+        title: "Seven Seas Of Rhye",
+        title_short: "Seven Seas Of Rhye",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996974",
+        duration: 189,
+        rank: 154804,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-7.deezer.com/stream/c-73bde68974f0020e74dd9699548b175d-17.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996975,
+        readable: true,
+        title: "Nevermore",
+        title_short: "Nevermore",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996975",
+        duration: 87,
+        rank: 158771,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-5.deezer.com/stream/c-554bf3a61af82bb47a9573b88017b3b8-18.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+      {
+        id: 9996976,
+        readable: true,
+        title: "See What A Fool I\u2019ve Been",
+        title_short: "See What A Fool I\u2019ve Been",
+        title_version: "",
+        link: "http://www.deezer.com/track/9996976",
+        duration: 271,
+        rank: 172130,
+        explicit_lyrics: false,
+        explicit_content_lyrics: 0,
+        explicit_content_cover: 0,
+        preview: "http://cdn-preview-8.deezer.com/stream/c-820a6e1af69f2bb59764a747fbca49e6-17.mp3",
+        md5_image: "2b8899e10a2c8e7021b907697b2e5010",
+        artist: { id: 412, name: "Queen", tracklist: "http://api.deezer.com/artist/412/top?limit=50", type: "artist" },
+        type: "track",
+      },
+    ],
+  },
+};
+
+const playerSongName = document.getElementById("player-song-name");
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const prevTrack = document.getElementById("prev-track");
+const nextTrack = document.getElementById("next-track");
+const currentTime = document.getElementById("current-time");
+const totalTime = document.getElementById("total-time");
+const seekBar = document.getElementById("seek-bar");
+let currentTrack = document.createElement("audio");
+currentTrack.src = albumData.tracks.data[0].preview;
+currentTrack.load();
+let isPlaying = false;
+let trackIndex = 0;
+
+const playTrackButton = document.getElementsByClassName("song-id");
+function pickTrack(event) {
+  trackIndex = parseInt(event.target.innerHTML) - 1;
+  loadTrack();
+}
+
+function loadTrack() {
+  for (let i = 0; i < playTrackButton.length; i++) {
+    playTrackButton[i].classList.remove("now-playing");
+  }
+  playTrackButton[trackIndex].classList.add("now-playing");
+  playerSongName.innerText = albumData.tracks.data[trackIndex].title;
+  currentTrack.src = albumData.tracks.data[trackIndex].preview;
+  currentTrack.load();
+  playTrack();
+}
+
+function playTrack() {
+  if (isPlaying === false) {
+    pauseButton.classList.toggle("d-none");
+    playButton.classList.toggle("d-none");
+  }
+  isPlaying = true;
+  currentTrack.play();
+}
+
+function pauseTrack() {
+  currentTrack.pause();
+  isPlaying = false;
+  pauseButton.classList.toggle("d-none");
+  playButton.classList.toggle("d-none");
+}
+
+function playNextTrack() {
+  if (isPlaying === true && trackIndex < playTrackButton.length - 1) {
+    trackIndex++;
+    loadTrack();
+  }
+}
+
+function playPrevTrack() {
+  if (isPlaying === true && trackIndex > 0) {
+    trackIndex--;
+    loadTrack();
+  }
+}
+
+for (let i = 0; i < playTrackButton.length; i++) {
+  playTrackButton[i].addEventListener("click", pickTrack);
+}
+playButton.addEventListener("click", playpauseTrack);
+pauseButton.addEventListener("click", playpauseTrack);
+nextTrack.addEventListener("click", playNextTrack);
+prevTrack.addEventListener("click", playPrevTrack);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function resetValues() {
+  currentTime.textContent = "00:00";
+  totalTime.textContent = "00:30";
+  seek_slider.value = 0;
+}
